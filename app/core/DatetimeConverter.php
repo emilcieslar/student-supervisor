cb87130b6c165bbb9cea046b75e020173f7973c3
Emil Cieslar <emil.cieslar@gmail.com>
2015-08-08 22:22:58 +0100

    DatetimeConverter.php refactored

--- app/core/DatetimeConverter.php
+++ app/core/DatetimeConverter.php
@@ -1,7 +1,17 @@
 <?php
 
+/**
+ * Class DatetimeConverter
+ * This class is used to convert datetime into a specific formats
+ */
 class DatetimeConverter
 {
+    /**
+     * Converts $datetime into a provided format
+     * @param $datetime the provided datetime
+     * @param string $format the requested format
+     * @return string the converted format of datetime
+     */
     public static function getUserFriendlyDateTimeFormat($datetime, $format = 'j F Y, H:i')
     {
         # Old format (just as a reminder): $format = 'd-m-Y H:i'
@@ -9,6 +19,11 @@ class DatetimeConverter
         return $date->format($format);
     }
 
+    /**
+     * Coverts $datetime into Google friendly format
+     * @param $datetime the provided datetime
+     * @return string the Google friendly datetime
+     */
     public static function getGoogleAuthDateTimeFormat($datetime)
     {
         $date = DateTime::createFromFormat('Y-m-d H:i:s', $datetime);
