6b705a8bd1c05ee3775bf999059fcc0ed07ee619
Emil Cieslar <emil.cieslar@gmail.com>
2015-08-30 19:22:27 +0100

    Method for checking for google login in index.php that was uncommented for a long time was removed.

--- index.php
+++ index.php
@@ -9,24 +9,5 @@ if(isset($_GET['code']))
     GoogleAuth::getInstance()->checkCode($_GET['code']);
 }
 
-/*
-# Is user logged in?
-if(isset($_SESSION['access_token']) && $_SESSION['access_token'])
-{
-    # If access_token has expired
-    if($client->isAccessTokenExpired())
-    {
-        # We have to display a home page to user to log in again
-        unset($_SESSION['access_token']);
-        header('Location: /master/student-supervisor');
-    } else {
-        echo "logout";
-    }
-
-} else {
-    # No!
-    echo $googleAuth->getAuthLink();
-}*/
-
 # Start up the app
 $app = new App;
